fec465ba092c3a635e6098966ddb3b7a2e7513d2
Jason Chen <windson7@gmail.com>
2025-03-22 22:15:07 -0700

    callback hell in the flesh with chaining all previous functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
 const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -11,20 +12,32 @@ const { fetchISSFlyOverTimes } = require('./iss');
 //   console.log('It worked! Returned IP:' , ip);
 // });
 
-// fetchCoordsByIP('24.80.48.124', (error, data) => {
+// fetchCoordsByIP('24.80.48.124', (error, pas) => {
 //   if (error) {
 //     console.log(`It didn't work! `, error.message);
 //     return;
 //   }
 
-//   console.log(`It worked! Your geolocation: {latitude: ${data.latitude}, longitude: ${data.longitude}}`);
+//   console.log(`It worked! Your geolocation: {latitude: ${pas.latitude}, longitude: ${pas.longitude}}`);
 // });
 
-// fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' }, (error, data) => {
+// fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' }, (error, pas) => {
 //   if (error) {
 //     console.log(`It didn't work! `, error.message);
 //     return;
 //   }
 
-//   console.log(`It worked!`, JSON.stringify(data, null, 2));
+//   console.log(`It worked!`, JSON.stringify(pas, null, 2));
 // });
+
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  for (let i = 0; i < passTimes.length; i++) {
+    console.log(`Next pass at ${Date(passTimes[i].risetime)} for ${passTimes[i].duration} seconds!`);
+  }
+});
+
